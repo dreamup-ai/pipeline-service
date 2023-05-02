65db740c93c72e5be6d6afb344c35d91e37eb624
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2023-05-02 09:46:22 -0400

    tests for validate endpoint

--- test/routes/validate.ts
+++ test/routes/validate.ts
@@ -137,9 +137,75 @@ describe("POST /pipeline/:id/validate", () => {
     expect(response.json()).to.deep.equal({ error: "Missing signature" });
   });
 
-  it("returns 401 if session token is invalid");
+  it("returns 401 if session token is invalid", async () => {
+    const payload = {
+      input: {
+        test: "test",
+      },
+      output: {
+        test: "test",
+      },
+    };
+    const response = await server.inject({
+      method: "POST",
+      url: `/pipeline/${pipeline.id}/validate`,
+      headers: {
+        authorization: "Bearer invalid",
+      },
+      payload,
+    });
 
-  it("returns 401 if signature is present and invalid");
+    expect(response.statusCode).to.equal(401);
+    expect(response.json()).to.deep.equal({
+      error: "Invalid authorization token",
+    });
+  });
 
-  it("returns 404 if pipeline does not exist");
+  it("returns 401 if signature is present and invalid", async () => {
+    const payload = {
+      input: {
+        test: "test",
+      },
+      output: {
+        test: "test",
+      },
+    };
+    const response = await server.inject({
+      method: "POST",
+      url: `/pipeline/${pipeline.id}/validate`,
+      headers: {
+        [config.webhooks.signatureHeader]: "invalid",
+      },
+      payload,
+    });
+
+    expect(response.statusCode).to.equal(401);
+    expect(response.json()).to.deep.equal({
+      error: "Invalid signature",
+    });
+  });
+
+  it("returns 404 if pipeline does not exist", async () => {
+    const payload = {
+      input: {
+        test: "test",
+      },
+      output: {
+        test: "test",
+      },
+    };
+    const response = await server.inject({
+      method: "POST",
+      url: `/pipeline/invalid/validate`,
+      headers: {
+        [config.webhooks.signatureHeader]: sign(JSON.stringify(payload)),
+      },
+      payload,
+    });
+
+    expect(response.statusCode).to.equal(404);
+    expect(response.json()).to.deep.equal({
+      error: "Pipeline not found",
+    });
+  });
 });
